18e5c7b91771f12a424957c336140b87e181cab0
scarstreet <scarlettstrid@gmail.com>
2021-08-22 18:18:20 +0800

    Changed main page

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <h1>I made this web OwO)...</h1>
+    <p>I hab my own domain OwO).........</p>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
